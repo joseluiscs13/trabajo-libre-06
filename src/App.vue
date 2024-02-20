84e4ca8b256cf85f0bea16c77e6165bafc602b60
José Luis Camarena <149542705+joseluiscs13@users.noreply.github.com>
2024-02-19 21:22:18 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- Contenedor principal de la aplicación -->
+  <div id="app" class="app-background">
+    <!-- Encabezado de la aplicación -->
+    <div class="header">
+      <!-- Imagen del logo de la aplicación -->
+      <img src="@/assets/logo.jpeg" alt="Logo de la aplicación" width="200" height="200" />
+      <br /><br />
+      <!-- Título de la aplicación -->
+      <h1>DigiSolver</h1>
+    </div>
+    <!-- Componente de la calculadora -->
+    <Calculator />
+  </div>
+</template>
+
+<script>
+// Importa el componente Calculator desde el archivo correspondiente
+import Calculator from './components/Calculator.vue';
+
+// Define el componente principal de la aplicación
+export default {
+  // Registra el componente Calculator para su uso en este componente
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+/* Estilos para el contenedor principal de la aplicación */
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+
+/* Estilos para el fondo de la aplicación */
+.app-background {
+  background-image: url("./assets/fondo.jpg"); /* Ruta de tu imagen de fondo */
+  background-size: cover; /* Ajusta la imagen al tamaño del contenedor */
+  background-position: center; /* Centra la imagen */
+  background-repeat: no-repeat; /* Evita la repetición de la imagen */
+  min-height: 100vh; /* Garantiza que el fondo ocupe al menos el 100% de la altura de la ventana */
+  display: flex;
+  flex-direction: column;
+}
+
+/* Estilos para el encabezado de la aplicación */
+.header {
+  margin-bottom: 20px;
+}
+
+/* Estilos para el título de la aplicación */
+.header h1 {
+  font-size: 1.5em;
+  margin-bottom: 10px;
+}
+
+/* Estilos para la imagen del logo */
+.header img {
+  width: 200px;
+  height: 200px;
+}
+</style>
+
+
---
+++ src/components/Calculator.vue
@@ -0,0 +1,118 @@
+<template>
+      <!-- Contenedor principal de la calculadora -->
+    <div class="calculator">
+       <!-- Pantalla de la calculadora -->
+        <div class="calculator-screen">
+        <!-- Input de texto para mostrar el valor actual -->
+        <input type="text" :value="display" disabled />
+      </div>
+         <!-- Botones de la calculadora -->
+      <div class="calculator-buttons">
+        <button @click="appendToDisplay('1')">1</button>
+        <button @click="appendToDisplay('2')">2</button>
+        <button @click="appendToDisplay('3')">3</button>
+        <button @click="appendToDisplay('4')">4</button>
+        <button @click="appendToDisplay('5')">5</button>
+        <button @click="appendToDisplay('6')">6</button>
+        <button @click="appendToDisplay('7')">7</button>
+        <button @click="appendToDisplay('8')">8</button>
+        <button @click="appendToDisplay('9')">9</button>
+        <button @click="appendToDisplay('0')">0</button>
+        <button @click="appendToDisplay('+')">+</button>
+        <button @click="appendToDisplay('-')">-</button>
+        <button @click="appendToDisplay('*')">x</button>
+        <button @click="appendToDisplay('/')">/</button>
+        <button @click="appendToDisplay('Math.sqrt(')">√</button>
+        <button @click="appendToDisplay('**')">^</button>
+        <button @click="clearDisplay()">C</button>
+        <button @click="calculateResult()">=</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        // Valor actual en la pantalla de la calculadora
+        display: '0',
+      };
+    },
+    methods: {
+      // Función para agregar un valor a la pantalla
+        appendToDisplay(value) {
+        if (this.display === '0' || this.display === 'Error') {
+          this.display = value;
+        } else {
+          this.display += value;
+        }
+      },
+      // Función para borrar la pantalla
+      clearDisplay() {
+        this.display = '0';
+      },
+       // Función para calcular el resultado de la expresión en la pantalla
+      calculateResult() {
+        try {
+          this.display = eval(this.display);
+        } catch (error) {
+          this.display = 'Error';
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos para la calculadora */
+.calculator {
+  max-width: 300px;
+  margin: auto;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  background-color: #000; /* Fondo negro */
+  color: #fff; /* Texto blanco */
+}
+
+/* Estilos para la pantalla de la calculadora */
+.calculator-screen {
+  margin-bottom: 10px;
+  padding: 8px;
+  font-size: 1.5em;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: right;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  outline: none;
+  background-color: #ccc; /* Fondo negro más oscuro */
+  color: #fff; /* Texto blanco */
+}
+
+/* Estilos para los botones de la calculadora */
+.calculator-buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 5px;
+}
+
+.calculator-buttons button {
+  width: 100%;
+  height: 50px;
+  font-size: 1.2em;
+  background-color: #333; /* Fondo negro aún más oscuro */
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  color: #fff; /* Texto blanco */
+}
+
+.calculator-buttons button:hover {
+  background-color: #555; /* Cambio de color al pasar el ratón */
+}
+</style>
+
+  
